436bc21a83c4cd28a0139b396328c4940bcfbac4
Mahdi Esmaili Jadidi <m.esmailijadidi@gmail.com>
2025-09-06 23:18:29 +0330

    Inputs almost finished

--- Fudamental_Prj404/Models/User.cs
+++ Fudamental_Prj404/Models/User.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,54 @@ namespace Fudamental_Prj404.Models
 {
     internal class User
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string NationalCode { get; set; }
+        public DateTime BirthDate { get; set; }
+        public uint Age
+        {
+            get
+            {
+                var today = DateTime.Today;
+                var age = (uint)(today.Year - BirthDate.Year);
+                if (BirthDate > today.AddYears(-(int)age)) age--;
+                return age;
+            }
+        }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        //public User(string firstName, string lastName, string email, string password)
+        //{
+        //    FirstName = firstName;
+        //    LastName = lastName;
+        //    Email = email;
+        //    Password = password;
+        //}
+        //public uint Age => 
+        public override string ToString()
+        {
+            return $"FullName: {FullName}, Email: {Email}";
+        }
+        public bool CheckPassword(string password)
+        {
+            return Password == password;
+        }
+        public void ChangePassword(string oldPassword, string newPassword)
+        {
+            if (CheckPassword(oldPassword))
+            {
+                Password = newPassword;
+            }
+            else
+            {
+                throw new Exception("Old password is incorrect");
+            }
+        }
+        public void ChangeEmail(string newEmail)
+        {
+            Email = newEmail;
+        }
     }
 }
--- Fudamental_Prj404/Program.cs
+++ Fudamental_Prj404/Program.cs
@@ -1,10 +1,128 @@
-﻿namespace Fudamental_Prj404
+﻿using Fudamental_Prj404.Models;
+
+namespace Fudamental_Prj404
 {
     internal class Program
     {
+        public static void WelcomeMsj()
+        {
+            Console.WriteLine("Welcome to the MLine system");
+        }
+
+        public static void InputsData (User user)
+        {
+            do // Inputs first name
+            {
+                Console.Write("Please enter your first name : ");
+                string FirstName = Console.ReadLine();
+                if (string.IsNullOrEmpty(FirstName) || string.IsNullOrWhiteSpace(FirstName))
+                    Console.WriteLine("First name cannot be empty");
+                else
+                {
+                    //first letter to upper case
+                    user.FirstName = char.ToUpper(FirstName[0]) + FirstName.Substring(1).ToLower();
+                    break;
+                }
+            } while (true);
+
+            do // Inputs last name
+            {
+                Console.Write("Please enter your last name : ");
+                string LastName = Console.ReadLine();
+                if (string.IsNullOrEmpty(LastName) || string.IsNullOrWhiteSpace(LastName))
+                    Console.WriteLine("Last name cannot be empty");
+                else
+                {
+                    //first letter to upper case
+                    user.LastName = char.ToUpper(LastName[0]) + LastName.Substring(1).ToLower();
+                    break;
+                }
+            } while (true);
+
+            do // Inputs email
+            {
+                Console.Write("Please enter your email : ");
+                string Email = Console.ReadLine();
+                if (string.IsNullOrEmpty(Email) || string.IsNullOrWhiteSpace(Email))
+                    Console.WriteLine("Email cannot be empty");
+                else if (!Email.Contains("@") || !Email.Contains(".") || Email.StartsWith("@") || Email.StartsWith("."))
+                    Console.WriteLine("Email is not valid");
+                else
+                {
+                    user.Email = Email;
+                    break;
+                }
+            } while (true);
+
+            do // Inputs phone number
+            {
+                Console.Write("Please enter your phone number : ");
+                string PhoneNumber = Console.ReadLine();
+                if (string.IsNullOrEmpty(PhoneNumber) || string.IsNullOrWhiteSpace(PhoneNumber))
+                    Console.WriteLine("Phone number cannot be empty");
+                else if (PhoneNumber.Length != 11 || !PhoneNumber.StartsWith("09") || !long.TryParse(PhoneNumber, out _))
+                    Console.WriteLine("Phone number is not valid");
+                else
+                {
+                    user.PhoneNumber = PhoneNumber;
+                    break;
+                }
+            } while (true);
+
+            do // Inputs National Code
+            {
+                Console.Write("Please enter your national code : ");
+                string NationalCode = Console.ReadLine();
+                if (string.IsNullOrEmpty(NationalCode) || string.IsNullOrWhiteSpace(NationalCode))
+                    Console.WriteLine("National code cannot be empty");
+                else if (NationalCode.Length != 10 || !long.TryParse(NationalCode, out _))
+                    Console.WriteLine("National code is not valid");
+                else
+                {
+                    user.NationalCode = NationalCode;
+                    break;
+                }
+            } while (true);
+
+            do //Inputs Birth Date
+            {
+                Console.Write("Please enter your birth date (yyyy/mm/dd) : ");
+                string BirthDate = Console.ReadLine();
+                if (string.IsNullOrEmpty(BirthDate) || string.IsNullOrWhiteSpace(BirthDate))
+                    Console.WriteLine("Birth date cannot be empty");
+                else if (!DateTime.TryParse(BirthDate, out DateTime birthDate))
+                    Console.WriteLine("Birth date is not valid");
+                else
+                {
+                    user.BirthDate = birthDate;
+                    break;
+                }
+            } while (true);
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WelcomeMsj();
+
+            Console.WriteLine("<<< You have to sign up in advance >>>");
+            Console.WriteLine("Please fill in the following information to sign up:");
+            Console.WriteLine("<--------------------------------------------------->");
+            User user = new User();
+
+            InputsData(user); // Input all user data
+
+            // Double check the inputs with user
+            Console.WriteLine("<--------------------------------------------------->");
+            Console.WriteLine("Accepted, Let's double check the result");
+            Console.WriteLine("<--------------------------------------------------->");
+            Console.WriteLine($"First name : {user.FirstName}");
+            Console.WriteLine($"Last name : {user.LastName}");
+            Console.WriteLine($"National code : {user.NationalCode}");
+            Console.WriteLine($"Email : {user.Email}");
+            Console.WriteLine($"Phone number : {user.PhoneNumber}");
+            Console.WriteLine($"Birth date : {user.BirthDate.ToShortDateString()}, You are {user.Age} years old");
+            Console.WriteLine("<--------------------------------------------------->");
+
         }
     }
 }
