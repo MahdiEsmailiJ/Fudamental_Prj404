9aadf7281f9707d837da39d0400af2821cc9a856
Mahdi Esmaili Jadidi <m.esmailijadidi@gmail.com>
2025-09-06 22:19:04 +0330

    Add project files.

---
+++ Fudamental_Prj404/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Fudamental_Prj404
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
